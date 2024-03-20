bcd24dc0da7b012ed8bad63d8aba31b16e690494
seentry <146814298+seentry@users.noreply.github.com>
2024-03-20 22:57:53 +0100

    Add files via upload

---
+++ class/modelo.php
@@ -0,0 +1,46 @@
+<?php
+
+class Modelo extends Connection{
+    public function __construct() {
+        parent::__construct();        
+    }    
+    public function getAllProductos(){
+
+    }
+    public function showAllProductos(){
+        
+    }
+    public function getAllEmp(){
+
+    }
+    public function showAllEmp(){
+        
+    }
+    public function getAllClients($order){
+
+    }
+    public function showAllClients($order){
+        
+    }
+    public function getPedidoOver($total){
+
+    }
+    public function showPedidoOver($total){
+        
+    }
+    public function getLineasPedido($pedido){
+        
+    }
+    public function getLineasPedidoMayor($pedido){
+        
+    }
+    public function showLineasPedido($pedido){
+        
+    }
+    public function showPaginator($pedido){
+        
+    }
+    
+
+
+}
